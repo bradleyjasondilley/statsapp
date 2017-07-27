cc1cc85bded0b146d6668a5731e7becdfd162e38
Solbrad <solbrad@gmail.com>
2017-07-27 21:24:31 +1200

    adding new charts

--- controllers/UsrStatsCtrl.js
+++ controllers/UsrStatsCtrl.js
@@ -16,7 +16,12 @@ angular.module('UserStats').controller('UsrStatsCtrl', function ($scope,$http,Da
       ]
     };
 
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A'];
 
+    $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40]
+    ];
 
     var usrUrl = 'assets/data/userInfo.json';
 
